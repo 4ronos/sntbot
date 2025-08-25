b63899f40c494210cffc32f0dc59aca13ee16070
4ronos <vavelka333@gmail.com>
2025-08-25 16:50:02 +0300

    db fix

--- bot/handlers/user/subscription.py
+++ bot/handlers/user/subscription.py
@@ -2,6 +2,7 @@ import logging
 from aiogram import Router, F, types, Bot
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from typing import Optional, Dict, Any, Union
 from datetime import datetime, timezone
 from sqlalchemy.ext.asyncio import AsyncSession
